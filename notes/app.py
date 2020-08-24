7959eb17d894628bbb75651756877efa7162d617
Midhun Manohar <midhmanohar@gmail.com>
2020-08-24 17:06:26 +0530

    routes added

--- notes/app.py
+++ notes/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
@@ -9,27 +9,82 @@ api = Api(app)
 
 
 # sample data
-NOTES = {
-    1: {
+NOTES = [
+    {
+        "id":1,
+        "title": "Sample title",
+        "body": "Sample body",
+        "author": "John",
+    },
+    {
+        "id":2,
+        "title": "Sample title",
+        "body": "Sample body",
+        "author": "John",
+    },
+    {
+        "id":3,
+        "title": "Sample title",
+        "body": "Sample body",
+        "author": "John",
+    },
+    {
+        "id":4,
         "title": "Sample title",
         "body": "Sample body",
         "author": "John",
     }
-}
+]
 
 
 class Note(Resource):
     def get(self, note_id):
-        return NOTES[note_id]
+        return jsonify(NOTES[note_id])
 
     def delete(self, note_id):
         del NOTES[note_id]
         return '', 204
 
+    def put(self,note_id):
+        new_id = note_id
+        new_title = request.form['title']
+        new_body = request.form['body']
+        new_author = request.form['author']
+
+        new_obj = {
+          "id":new_id,
+          "title":new_title,
+          "body":new_body,
+          "author":new_author
+        }
+
+        NOTES.append(new_obj)
+
+        return jsonify(NOTES)
+
+
 
 class NoteList(Resource):
     def get(self):
-        return NOTES
+        return jsonify(NOTES)
+
+    def post(self):
+        new_id = request.form['id']
+        new_title = request.form['title']
+        new_body = request.form['body']
+        new_author = request.form['author']
+
+        new_obj = {
+          "id":new_id,
+          "title":new_title,
+          "body":new_body,
+          "author":new_author
+        }
+
+        NOTES.append(new_obj)
+
+        return jsonify(NOTES),201
+
 
 
 api.add_resource(Note, '/notes/<int:note_id>')
